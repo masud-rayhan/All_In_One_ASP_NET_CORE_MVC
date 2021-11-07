84b4ff70bef975527c4d86b550e5de03bde2249f
Masud Rayhan <masudrayhan@yahoo.com>
2021-11-07 11:35:10 +0600

    Login And Registretion update

---
+++ All_In_One/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using All_In_One.DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(All_In_One.Areas.Identity.IdentityHostingStartup))]
+namespace All_In_One.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
